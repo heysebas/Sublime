e07178b791ab96fb0381cd9e6684e2c9774cd22f
Heysebas <sebazgrisales@gmail.com>
2024-07-10 10:34:37 -0500

    Temas

--- src/app/pages/layout/navbar/navbar.component.ts
+++ src/app/pages/layout/navbar/navbar.component.ts
@@ -1,18 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  @HostBinding('class') className = ''; // Esta variable de clase se usará para cambiar dinámicamente las clases CSS
+
+  darkMode: boolean = false; // Inicialmente asume que no está en modo oscuro
+
   menuOpen = false;
 
-  toggleMenu(){
-    this.menuOpen = !this.menuOpen;
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Detectar el tema del navegador
+      const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+      this.darkMode = prefersDark;
+
+      // Asignar clases CSS dinámicamente basadas en el tema
+      this.className = this.darkMode ? 'dark-theme' : 'light-theme';
+
+      // Escuchar cambios en el tema
+      window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+        this.darkMode = e.matches;
+        this.className = this.darkMode ? 'dark-theme' : 'light-theme';
+      });
+    }
   }
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
